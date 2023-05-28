894bd169a2f6368e135f3b32da499afd4add67b6
João <joaovicenterangel@gmail.com>
2023-05-28 20:48:53 -0300

    Criando a primeira model e suas respectivas anotações

---
+++ src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -0,0 +1,58 @@
+package br.com.alurafood.pagamentos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+Anotação @Entity para identificar que a classe será ligada ao banco. Caso a anotação não especifique o banco a ser utilizado,
+ele chamará o banco que tem exatamente o mesmo nome da classe. A anotação @Table especifica a tabela a ser utilizada.
+ */
+@Entity
+@Table(name = "pagamentos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+
+    @NotBlank
+    @Size(max=100)
+    private String nome;
+
+    @NotBlank
+    @Size(max=19)
+    private String numero;
+
+    @NotBlank
+    @Size(max=7)
+    private String expiracao;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String codigo;
+
+    @NotNull
+    @Enumerated(EnumType.STRING) // Identifica que a classe Status é um Enum, do tipo String
+    private Status status;
+
+    @NotNull
+    private Long pedidoId;
+
+    @NotNull
+    private Long formaDePagamentoId;
+}
